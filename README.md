fe94493e8d3d5435c77d474e0a124964ee1c4e2e
Luis Alberto Loyola <luisalbertoloyola@gmail.com>
2020-06-22 00:17:01 -0400

    edit readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# my_store
+# MyStore
+
+## Project Description
+
+MyStore is a streetwear e-commerce. Its functionality is based on Javascript Framework **Vue.js**, along with compatible components of this framework such as **Vue Router, VUEXTesting E2E, Firebase and CSS style framework Bulma.**
+
+To see more of my work, please visit my GitHub Page https://github.com/LuisAlbertoLoyola
+
+_Keywords: Vue, Vue Router, VUEX, Testing, Firebase, Framework, Bulma, CSS, JavaScript, HTML, Git, Repository._
+
+## Steps to run the Project
 
 ## Project setup
 ```
@@ -35,5 +45,13 @@ npm run test:e2e
 npm run test:unit
 ```
 
+## Link Deploy Hosting
+
+https://tgdd3-f199f.web.app/
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Author
+
+**_Luis Alberto Loyola_**
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,50 +1,52 @@
 <template>
     <div>
-        <div class="columns">
-            <div class="column is-one-third"></div>
-            <div class="column is-one-third">
-                <div class="login-container card">
-                    <form>
-                        <div>
-                            <h1 class="title text-center">Register</h1>
-                        </div>
-                        <!-- Name Field -->
-                        <div class="field">
-                            <label class="label">Name</label>
-                            <div>
-                                <input type="text" placeholder="Please enter your name" class="input">
-                            </div>
-                        </div>
-                        <!-- Email Field -->
-                        <div class="field">
-                            <label class="label">Email</label>
-                            <div>
-                                <input type="email" placeholder="Please enter your email" class="input">
-                            </div>
-                        </div>
-                        <!-- Password Field -->
-                        <div class="field">
-                            <label class="label">Password</label>
-                            <div>
-                                <input type="password" placeholder="Please enter your password" class="input">
-                            </div>
-                        </div>
-                        <!-- Checkbox -->
-                        <div>
-                            <input type="checkbox" v-model='isAccepted'>
-                        </div>
-                        <!-- Submit Field-->
-                        <div class="field">
-                            <button type="submit" :class='{"is-disabled": !isAccepted}'
-                                @click='submitForm'>Registrar</button>
-                        </div>
-                        <div class="is-clearfix"></div>
-                    </form>
-                </div>
+    <div class="columns" style="height: 70vh">
+      <div class="column is-one-third"></div>
+      <div class="column is-one-third" id="container">
+        <div class="login-container card">
+          <form>
+            <!-- Field email -->
+            <div class="field">
+              <!-- Error -->
+              <span v-if="formHasErrors" class="has-text-danger">
+                <i class="mdi mdi-alert"></i>
+                Incorrect User or Password. Try again.
+              </span>
             </div>
-            <div class="column is-one-third"></div>
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control has-icons-left has-icons-right">
+                <input type="email" name="email" id="email" placeholder="Email"
+                v-model="credentials.email"
+                class="input">
+                <span class="icon is-small is-left">
+                  <i class="mdi mdi-email"></i>
+                </span>
+              </div>
+            </div>
+            <!-- Field password -->
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control has-icons-left has-icons-right">
+                <input type="password" name="password" id="password" placeholder="*******"
+                v-model="credentials.password"
+                class="input">
+                <span class="icon is-small is-left">
+                  <i class="mdi mdi-key"></i>
+                </span>
+              </div>
+            </div>
+            <!-- field submit -->
+            <div class="field">
+              <button class="button is-primary is-pulled-right" @click="login">Login</button>
+            </div>
+            <div class="is-clearfix"></div>
+          </form>
         </div>
+      </div>
+      <div class="column is-one-third"></div>
     </div>
+  </div>
 </template>
 
 <script>
